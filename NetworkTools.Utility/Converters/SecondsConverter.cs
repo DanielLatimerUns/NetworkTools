6ed52f825d7da3c5f3981c257276972e637d9913
Dominick Unsworth <domxx9@gmail.com>
2019-02-04 16:54:48 +0000

    Multi: Settings and Torrent-controler

---
+++ NetworkTools.Utility/Converters/SecondsConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkTools.Utility.Converters
+{
+    public static class SecondsConverter
+    {
+        static readonly string[] TimeSuffixes = { "S", "M", "H","D"};
+
+        public static string TimeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + TimeSuffix(-value); }
+
+            int i = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 60)
+            {
+                dValue /= 60;
+                i++;
+            }
+
+            var retunrString = string.Format("{0:n" + decimalPlaces + "} {1}", dValue, TimeSuffixes[i]);
+            return retunrString;
+        }
+    }
+}
---
+++ NetworkTools/ClientApp/src/app/models/settingsModel.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=settingsModel.js.map
