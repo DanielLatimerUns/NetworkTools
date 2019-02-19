10470a8493cc4b5104c3e75725be24b48fb4398c
Dominick Unsworth <domxx9@gmail.com>
2019-02-19 18:04:08 +0000

    Functionality: Started on multiscreen interface.

--- NetworkTools/ClientApp/src/app/components/settings/settings.component.ts
+++ NetworkTools/ClientApp/src/app/components/settings/settings.component.ts
@@ -5,26 +5,63 @@ import { AppState } from '../../state/appState';
 import { ActionModel } from '../../models/actionModel';
 
 @Component({
-    selector: 'app-settings',
-    templateUrl: './settings.component.html',
-    styleUrls: ['./settings.component.css'],
-    providers: [ TorrentService ]
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css'],
+  providers: [TorrentService]
 })
 export class SettingsComponent {
+
+  public showGeneralWindow = true;
+  public showDownloadsWindow = false;
+  public showNetworkWindow = false;
+  public showPluginsWindow = false;
+  public showAutomationWindow = false;
+  public showSchedulerWindow = false;
+  public showLabelsWindow = false;
+  public showAdvancedWindow = false;
+  public showAboutWindow = false;
+
+
 
   constructor(private torrentService: TorrentService, state: AppState) {
 
     state.actionBarActions = [
-      new ActionModel(1, 'General', ""),
+      new ActionModel(1, 'General', "switchWindow(getCurrentwindow(),)"),
       new ActionModel(2, 'Downloads', ""),
       new ActionModel(3, 'Network', ""),
       new ActionModel(4, 'Plugins', ""),
       new ActionModel(5, 'Automation', ""),
       new ActionModel(6, 'Scheduler', ""),
-      new ActionModel(7, 'Lables', ""),
+      new ActionModel(7, 'Labels', ""),
       new ActionModel(8, 'Advanced', ""),
       new ActionModel(9, 'About', ""),
     ];
+  }
+
+  public getCurrentwindow() {
+    if (this.showGeneralWindow = true)
+      return 1
+    else if (this.showDownloadsWindow = true)
+      return 2
+    else if (this.showNetworkWindow = true)
+      return 3
+    else if (this.showPluginsWindow = true)
+      return 4
+    else if (this.showAutomationWindow = true)
+      return 5
+    else if (this.showSchedulerWindow = true)
+      return 6
+    else if (this.showLabelsWindow = true)
+      return 7
+    else if (this.showAdvancedWindow = true)
+      return 8
+    else if (this.showAboutWindow = true)
+      return 9
+  }
+
+  public switchWindow(currentWindow, windowState, newWindow) {
+
   }
 
 }
